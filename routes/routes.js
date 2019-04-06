f49a09f6811a9d4f2765ca004e94d5bf1b197321
adilsachwani <adilsachwani@gmail.com>
2019-04-06 19:02:11 +0500

    getTransactionReceipt function added in deploy_contract api

--- routes/routes.js
+++ routes/routes.js
@@ -4,25 +4,14 @@ const pool = require('../data/config');
 // Route the app
 const router = app => {
 
-    
     const Web3 = require('web3');
     const fs = require('fs');
     const solc = require('solc');
     const Tx = require('ethereumjs-tx');
     const web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:7545"));
 
-    const accountAddress = '0x13AB9be743BBBd271Ed766Fe20fc5c4Ed8a64F4C';
-    const PRIVATE_KEY_1 = Buffer.from('8F82CAADF1B631C1AFE402805F88D4DD001B5D35ECD2CC3F49F383128433E486', 'hex');
-    
-
-    var abi;
-    var contractAddress = "0xEF407d38e555a615011ad9c3D69C1529E3Ae3F78";
-    var contractTransaction;
-
-    var election_name ;
-    var election_date ;
-    var election_time ;
-    var election_duration ;
+    const accountAddress = '0x70a47E1Be460464bE8Dc17F2FDEEf2dC306f274d';
+    const privateKey = Buffer.from('d051b5b1e1cda01278161f21ddd71425f00cbd8081c841231b9ad794399c18f0', 'hex');
     
     // Display welcome message on the root
     app.get('/', (request, response) => {
@@ -34,106 +23,61 @@ const router = app => {
     // Display all users
     app.get('/deploy_contract', (request, response) => {
 
-        const input = fs.readFileSync('E:/Softwares/Xampp/htdocs/onevote/Election.sol');
+        const input = fs.readFileSync('C:/xampp/htdocs/onevote/Election.sol');
         const output = solc.compile(input.toString(), 1);
-        console.log(output);
         const bytecode = output.contracts[':Election'].bytecode;
         abi = JSON.parse(output.contracts[':Election'].interface);
 
         web3.eth.getTransactionCount(accountAddress, (err, txCount) => {
+            
             //build the transaction
             const txObject = {
                 nonce : web3.utils.toHex(txCount),
                 data : '0x' + bytecode,
-                gasLimit : web3.utils.toHex(10000000000),
+                gasLimit : web3.utils.toHex(1000000000),
                 gasPrice : web3.utils.toHex(web3.utils.toWei('10', 'gwei'))
             }
-            console.log(txObject);
 
             //Sign the transaction
             const tx = new Tx(txObject);
-            tx.sign(PRIVATE_KEY_1);
+            tx.sign(privateKey);
 
             const serializedTransaction = tx.serialize();
             const raw = '0x' + serializedTransaction.toString('hex');
 
-            // console.log("Serialized Transaction : ", serializedTransaction);
-            // console.log("Raw : ", raw);
-
             //Broadcast the address
             web3.eth.sendSignedTransaction(raw, (err, txHash) =>{
-                console.log("TxHash : ", txHash);
-                console.log("Error : ", err);
-            });
 
-        });
-
-
-
-
-
-        // // Contract object
-        // const contract = web3.eth.Contract(abi);
-
-        // // Deploy contract instance
-        // const contractInstance = contract.new({
-
-        //     data: '0x' + bytecode,
-        //     from: accountAddress,
-        //     gas: 90000000*2
-
-        // }, (err, res) => {
-
-        //     if (err) {
-        //         console.log(err);
-        //         return;
-        //     }
-
-        //     // If we have an address property, the contract was deployed
-        //     if (res.address) {
-
-        //         contractAddress = res.address;
-        //         contractTransaction = res.transactionHash;
+                web3.eth.getTransactionReceipt(txHash, (err, receipt) => {
+                    
+                    response.send({
+                        transaction_hash : receipt.transactionHash,
+                        contract_address : receipt.contractAddress,
+                        block_number : receipt.blockNumber,
+                        gas_used: receipt.gasUsed
+                    })
                 
-        //         response.send({
-        //             message: 'Contract Deployed',
-        //             contract_address: res.address,
-        //             contract_transaction_hash: res.transactionHash,
-        //             ABI : abi
-        //         });
-            
-        //     }
+                });
 
-        // });
-
+            });
+        
+        });
+    
     });
 
     //display contract details
-    app.get('/get_contract_details', (request, response) => {
+    app.get('/get_election_details', (request, response) => {
 
-        var contract = new web3.eth.Contract(abi, contractAddress);
-        console.log(contract);
-        contract.methods.getPosts().call({from : accountAddress}, (err, name)=>{
-            if(err == null){
-               // election_name = name;
-                console.log("Election Name : ", name);
-            }
-        })
-        
+        const contractAddress = '0x9a8b3Cf0c8125b74c63A56865e0AfA2698707eb4';
+        const contractABI = [{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"posts","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"total_posts","outputs":[{"name":"","type":"int256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"election_date","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"candidatesCount","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"postsCount","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidates","outputs":[{"name":"name","type":"string"},{"name":"voteCount","type":"uint256"},{"name":"postId","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"election_id","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"votersCount","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"election_duration","outputs":[{"name":"","type":"int256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"election_secret_key","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"voters","outputs":[{"name":"name","type":"string"},{"name":"email","type":"string"},{"name":"public_key","type":"string"},{"name":"vote","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"total_voters","outputs":[{"name":"","type":"int256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"election_time","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"election_name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"}];
 
-    });
+        var contract = new web3.eth.Contract(contractABI, contractAddress);
 
+        contract.methods.candidates(2).call({from : accountAddress}, (err, candidate) => {
+            console.log(candidate['name'])
+        });     
 
-    // Add a new user
-    app.post('/users', (request, response) => {
-        pool.query('INSERT INTO users SET ?', request.body, (error, result) => {
-            if (error) throw error;
-
-            response.status(201).send(`User added with ID: ${result.insertId}`);
-        });
-    });
-
-    
+    });    
 
 }
 
