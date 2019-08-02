b1e53fcf648ecd6f8be2dcd09b4b49c2894ce22f
adilsachwani <adilsachwani@gmail.com>
2019-08-02 09:35:42 +0500

    changed apis to rinkeby

--- routes/routes.js
+++ routes/routes.js
@@ -10,8 +10,9 @@ const router = app => {
     const Tx = require('ethereumjs-tx');
     const nodemailer = require('nodemailer');
     const dateFormat = require('dateformat');
-    const web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:7545"));
-
+    const url = "https://rinkeby.infura.io/v3/a2001c25b0d844b195b77d6aaf89074f";
+    const web3 = new Web3(new Web3.providers.HttpProvider(url));
+    
     const contractABI = [{"constant":true,"inputs":[],"name":"getCandidates","outputs":[{"components":[{"name":"cadidateId","type":"uint256"},{"name":"name","type":"string"},{"name":"voteCount","type":"uint256"},{"name":"postId","type":"uint256"}],"name":"","type":"tuple[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"posts","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"total_posts","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"election_date","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"candidatesCount","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"postsCount","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidates","outputs":[{"name":"cadidateId","type":"uint256"},{"name":"name","type":"string"},{"name":"voteCount","type":"uint256"},{"name":"postId","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getPosts","outputs":[{"name":"","type":"string[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"postId","type":"uint256"},{"name":"candidateId","type":"uint256"},{"name":"voterId","type":"uint256"}],"name":"castVote","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[],"name":"election_id","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"votersCount","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"election_duration","outputs":[{"name":"","type":"int256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getVoters","outputs":[{"components":[{"name":"name","type":"string"},{"name":"email","type":"string"},{"name":"public_key","type":"string"},{"name":"vote","type":"uint256[3]"},{"name":"hasVoted","type":"bool"}],"name":"","type":"tuple[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"voters","outputs":[{"name":"name","type":"string"},{"name":"email","type":"string"},{"name":"public_key","type":"string"},{"name":"hasVoted","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"total_voters","outputs":[{"name":"","type":"int256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"election_time","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"election_name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
 
     var transporter = nodemailer.createTransport({
@@ -52,9 +53,14 @@ const router = app => {
                 let voter_id;
 
                 for(let i=0; i<res.length; i++){
+
+                    console.log(res[i][2].toString());
+                    console.log(public_address);
+                    console.log(res[i][4]);
                     
                     if( res[i][2].toString() == public_address && res[i][4] == false) {
                         check = true;
+                        console.log("Matched");
                     }
 
                     if(check){
@@ -158,9 +164,8 @@ const router = app => {
 
 
     // Deploy election contract
-    app.get('/deploy_contract/:public_address/:private_key/:election_id', (request, response) => {
+    app.get('/deploy_contract/:public_address/:private_key', (request, response) => {
 
-        const election_id = request.params.election_id;
         const public_address = request.params.public_address;
         const private_key = Buffer.from(request.params.private_key, 'hex');
 
@@ -174,8 +179,10 @@ const router = app => {
             const txObject = {
                 nonce : web3.utils.toHex(txCount),
                 data : '0x' + bytecode,
-                gasLimit : web3.utils.toHex(1000000000),
-                gasPrice : web3.utils.toHex(web3.utils.toWei('10', 'gwei'))
+                gasLimit : web3.utils.toHex(2956678),
+                gasPrice : web3.utils.toHex(web3.utils.toWei('1', 'gwei')),
+                from : public_address,
+                chainId : 4
             }
 
             //Sign the transaction
@@ -188,80 +195,10 @@ const router = app => {
             //Broadcast the address
             web3.eth.sendSignedTransaction(raw, (err, txHash) =>{
 
-                web3.eth.getTransactionReceipt(txHash, (err, receipt) => {
-
-                    var query = "SELECT e.*, v.voter_email FROM election e INNER JOIN voter v ON e.election_id = v.election_id WHERE e.election_id = ?"; 
-
-                    pool.query(query, election_id, (error, result) => {
-
-                        if(error){
-                            
-                            console.log(error);
-                            
-                        } else{
-
-                            let emails = "";
-                            let title = result[0]['election_title'];
-                            let date = dateFormat(result[0]['election_date'], 'dS mmmm, yyyy');
-                            let time = timeConvert(result[0]['election_time']);
-
-                            for(var i=0 ; i<result.length; i++){
-
-                                if(i != result.length-1){
-                                    
-                                    emails += result[i]['voter_email'] + ",";
-                                
-                                } else {
-                                    
-                                    emails += result[i]['voter_email'];
-                                
-                                }
-                                
-                            }
-
-                            const mailOptions = {
-                                
-                                from: 'onevote.voting@gmail.com',
-                                to: emails,
-                                subject: 'OneVote Election Details - ' + title,
-                                html: 
-                                
-                                '<p>Dear Voter,</p>' +
-                                '<p>You have been registered for voting in <b>'+ title +'</b> on <b>' + date + '</b> at <b>' + time + '</b>.</p>' +
-                                '<p>Transaction Hash is <b>' + receipt.transactionHash + '</b></p>' +
-                                '<p>You can cast your vote through OneVote Webpage which is completely secured thorugh Blockchain: http://www.localhost/onevotehome</p>' +
-                                '<footer>' +
-                                    '<div>' +
-                                        '<a target="_blank" href="http://www.localhost:81/onevotehome">' +
-                                            '<img src="http://neditec.org.pk/images/onevote_email_footer.jpg" style="width:100%;" border="0" alt="Null">' +
-                                        '</a>' +
-                                    '</div>' +
-                                '</footer>'
-                            
-                            };
-
-                            transporter.sendMail(mailOptions, function (err, info) {
-                        
-                                if (err) {
-                                    console.log("Hello it's error time.");
-                                } else {
-                                    console.log('Email sent: ' + info.response);
-                                }
-                             });
-
-                             response.send({
-                                'transaction_hash' : receipt.transactionHash,
-                                'contract_address' : receipt.contractAddress,
-                                'block_number' : receipt.blockNumber,
-                                'gas_used' : receipt.gasUsed
-                            });
-
-                        }
-            
-                    });
-                
-                }).catch( (err) =>{
-                    console.log(err);
+                console.log(txHash);
+               
+                response.send({
+                    'transaction_hash' : txHash
                 });
 
             }).catch( (err) =>{
@@ -270,6 +207,78 @@ const router = app => {
         
         }).catch( (err) =>{
             console.log(err);
+        });
+    
+    });
+
+
+    // Send election transaction mail
+    app.get('/send_election_transaction_mail/:transaction_hash/:election_id', (request, response) => {
+
+        const transaction_hash = request.params.transaction_hash;
+        const election_id = request.params.election_id;
+
+        var query = "SELECT e.*, v.voter_email FROM election e INNER JOIN voter v ON e.election_id = v.election_id WHERE e.election_id = ?"; 
+
+        pool.query(query, election_id, (error, result) => {
+
+            if(error){
+                
+                console.log(error);
+                
+            } else{
+
+                let emails = "";
+                let title = result[0]['election_title'];
+                let date = dateFormat(result[0]['election_date'], 'dS mmmm, yyyy');
+                let time = timeConvert(result[0]['election_time']);
+
+                for(var i=0 ; i<result.length; i++){
+
+                    if(i != result.length-1){
+                        
+                        emails += result[i]['voter_email'] + ",";
+                    
+                    } else {
+                        
+                        emails += result[i]['voter_email'];
+                    
+                    }
+                    
+                }
+
+                const mailOptions = {
+                    
+                    from: 'onevote.voting@gmail.com',
+                    to: emails,
+                    subject: 'OneVote Election Details - ' + title,
+                    html: 
+                    
+                    '<p>Dear Voter,</p>' +
+                    '<p>You have been registered for voting in <b>'+ title +'</b> on <b>' + date + '</b> at <b>' + time + '</b>.</p>' +
+                    '<p>Transaction Hash is <b>' + transaction_hash + '</b></p>' +
+                    '<p>You can cast your vote through OneVote Webpage which is completely secured thorugh Blockchain: http://www.localhost/onevotehome</p>' +
+                    '<footer>' +
+                        '<div>' +
+                            '<a target="_blank" href="http://www.localhost/onevote">' +
+                                '<img src="http://neditec.org.pk/images/onevote_email_footer.jpg" style="width:100%;" border="0" alt="Null">' +
+                            '</a>' +
+                        '</div>' +
+                    '</footer>'
+                
+                };
+
+                transporter.sendMail(mailOptions, function (err, info) {
+            
+                    if (err) {
+                        console.log("Error sending mail.");
+                    } else {
+                        console.log('Email sent: ' + info.response);
+                    }
+                
+                });
+            }
+
         });
     
     });
@@ -422,42 +431,79 @@ const router = app => {
 
     });
 
-
     //Casting vote
-    app.get('/cast_vote/:transaction_hash/:public_address/:candidate_id/:post_id/:voter_id', (request, response) => {
+    app.get('/cast_vote/:transaction_hash/:public_address/:private_key/:candidate_id/:post_id/:voter_id', (request, response) => {
 
         const transaction_hash = request.params.transaction_hash;
         const public_address = request.params.public_address;
+        const private_key = Buffer.from(request.params.private_key, 'hex');
         const post_id = request.params.post_id;
         const voter_id = request.params.voter_id;
         const candidate_id = request.params.candidate_id;
 
+        console.log("post_id: " + post_id);
+        console.log("candidate_id: " + candidate_id);
+        console.log("voter_id: " + voter_id);
+
         web3.eth.getTransactionReceipt(transaction_hash, (err, receipt) => {
+
+            if(err){
+                console.log("Err 1: " + err);
+            }
 
             const contractAddress = receipt.contractAddress;
             const contract = new web3.eth.Contract(contractABI, contractAddress);
 
-            contract.methods.castVote(post_id,candidate_id,voter_id).send({from: public_address}, (error, transactionHash) => {
+            web3.eth.getTransactionCount(public_address, (err, txCount) => {
 
-                web3.eth.getTransactionReceipt(transactionHash, (err, receipt) => {
+                if(err){
+                    console.log("Err 2: " + err);
+                }
+
+                var method_data = contract.methods.castVote(post_id,candidate_id,voter_id);
+                var method_encoded_data = method_data.encodeABI();
+
+                console.log(web3.utils.toHex(txCount));
+            
+                //build the transaction
+                const txObject = {
                     
-                response.send({
-            
-                    'vote_transaction_hash' : receipt.transactionHash,
-                    'contract_address' : receipt.to,
-                    'receipt' : receipt
-                });
+                    nonce : web3.utils.toHex(txCount),
+                    data : method_encoded_data,
+                    gasLimit : web3.utils.toHex(2956678),
+                    gasPrice : web3.utils.toHex(web3.utils.toWei('10', 'gwei')),
+                    from : public_address,
+                    to: contractAddress,
+                    chainId : 4
+                
+                }
 
-                }).catch( (err) =>{
-                    console.log(err);
+                //Sign the transaction
+                const tx = new Tx(txObject);
+                tx.sign(private_key);
+
+                const serializedTransaction = tx.serialize();
+                const raw = '0x' + serializedTransaction.toString('hex');
+
+                //Broadcast the address
+                web3.eth.sendSignedTransaction(raw, (err, txHash) =>{
+
+                    if(err){
+                        console.log("Err 3: " + err);
+                    }
+
+                    console.log("txHash: " + txHash);
+
+                    response.send({
+                        'vote_transaction_hash' : txHash
+                    });
+
+                }).catch((err)=>{
+                    console.log("my error 2: " + err);
                 });
             
-            }).catch( (err) =>{
-                console.log(err);
             });
-    
-        }).catch( (err) =>{
-            console.log(err);
+
         });
 
     });
@@ -641,8 +687,8 @@ const router = app => {
     });
 
 
-    //Send transaction mail
-    app.post('/send_transaction_mail', (request, response) => {
+    //Send vote transaction mail
+    app.post('/send_vote_transaction_mail', (request, response) => {
 
         const email = request.body.email;
         const election_name = request.body.election_name;
@@ -665,10 +711,10 @@ const router = app => {
             '<p>Congratulations your vote(s) has been casted successfully.</p>' +
             '<p>Transaction Hash(s) are:' + hashes + '</p>' +
 
-            '<p>You can track your vote and see the results through OneVote Web App which is completely secured thorugh Blockchain: http://www.localhost/onevotehome/results</p>' +
+            '<p>You can track your vote and see the results through OneVote Web App which is completely secured thorugh Blockchain: http://www.localhost/onevote/result.html</p>' +
             '<footer>' +
                 '<div>' +
-                    '<a target="_blank" href="http://www.localhost:81/onevotehome">' +
+                    '<a target="_blank" href="http://www.localhost/onevote/vote.html">' +
                         '<img src="http://neditec.org.pk/images/onevote_email_footer.jpg" style="width:100%;" border="0" alt="Null">' +
                     '</a>' +
                 '</div>' +
@@ -691,8 +737,6 @@ const router = app => {
         });
 
     });
-
-
 
 
     function bigToNum(bigNum){
